09eea9bd711516481177705ab596e8a3adef7766
Devesh Khosla <dekhosla86@gmail.com>
2021-12-22 19:13:05 -0700

    Add project files.

---
+++ Grass/Grass/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Grass.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Grass.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Grass/Grass/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Grass.Models;
+using Grass.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Grass.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
